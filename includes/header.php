f9ff85fa008ba3bed3561694bd61754c4aa86c1d
Austin Chang <austin880625@gmail.com>
2019-03-29 02:11:55 +0800

    Split header.css from style.css
    把header專屬的css搬出來，從此style.css只放全域的字體或連結風格等特性

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
     <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" type="image/x-icon">
 
     <?php
+    wp_enqueue_style('header.css', get_template_directory_uri() . '/css/header.css');
     // wp_head()會引入在functions.php所指定的script，並且處理相依性產生的引入順序變化
     ?>
     <?php wp_head(); ?>
