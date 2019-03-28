bae31f12cbca47265a10d834de1f81b5c294087a
Austin Chang <austin880625@gmail.com>
2019-03-29 02:22:43 +0800

    Move language switcher out of searchform.php
    把語言切換的按鈕移到search form外面，讓search form就只是search form
    原本只是爲了寫css方面，其實搬出來也沒有特別難

--- includes/header.php
+++ includes/header.php
@@ -34,6 +34,10 @@ Top Navbar
                 </a>
             </div>
             <div class="top-search-wrapper">
+                <div class="top-navbar-lang-switch">
+                    <span class="btn-lang-switch lang-switch-ch">中文</span>
+                    <span class="btn-lang-switch lang-switch-en">English</span>
+                </div>
                 <?php get_search_form(); ?>
             </div>
         </div>
--- searchform.php
+++ searchform.php
@@ -1,10 +1,6 @@
 <form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <div class="top-navbar-lang-switch">
-        <span class="btn-lang-switch lang-switch-ch">中文</span>
-        <span class="btn-lang-switch lang-switch-en">English</span>
-    </div>
 	<div class="form-group">
-    <input class="form-control" type="text" value="<?php echo get_search_query(); ?>" placeholder="<?php echo __('Search...'); ?>" name="s" id="s">
+        <input class="form-control" type="text" value="<?php echo get_search_query(); ?>" placeholder="<?php echo __('Search...'); ?>" name="s" id="s">
 	</div>
 </form>
 
