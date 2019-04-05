7d99bb87158b1aae219044b8e8195eaa6f11935e
Austin Chang <austin880625@gmail.com>
2019-04-05 23:28:30 +0800

    Move Vue apps into app/
    因為每個Vue頁面內容不會只有js檔，所以移出到app/裡面

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,11 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 module.exports = {
   mode: NODE_ENV,
   entry: {
-    intro: path.join(__dirname, 'js/intro/index.js'),
-    news: path.join(__dirname, 'js/news/index.js'),
-    regulations: path.join(__dirname, 'js/regulations/index.js'),
-    punchclock: path.join(__dirname, 'js/punchclock/index.js'),
-    faq: path.join(__dirname, 'js/faq/index.js')
+    intro: path.join(__dirname, 'app/intro/index.js'),
+    news: path.join(__dirname, 'app/news/index.js'),
+    regulations: path.join(__dirname, 'app/regulations/index.js'),
+    punchclock: path.join(__dirname, 'app/punchclock/index.js'),
+    faq: path.join(__dirname, 'app/faq/index.js')
   },
 
   output: {
